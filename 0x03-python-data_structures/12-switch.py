17a035f5a9a346d943b650f3eda82259d122c94a
mrfaraa <elmehdifaraa@gmail.com>
2023-06-11 06:43:21 -0700

    task 12 switch

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    new_list = my_list[:]
-    if len(new_list) > idx >= 0:
-        new_list[idx] = element
-        return(new_list)
-    return(my_list)
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
