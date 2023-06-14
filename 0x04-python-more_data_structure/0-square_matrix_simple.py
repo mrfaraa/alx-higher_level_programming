80529f0ad198da02847231fabad7fc68bd61a289
mrfaraa <elmehdifaraa@gmail.com>
2023-06-14 15:49:54 -0700

    task 0 squared simple

--- 0x04-python-more_data_structure/0-square_matrix_simple.py
+++ 0x04-python-more_data_structure/0-square_matrix_simple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    nmatrix = [[x ** 2 for x in row] for row in matrix]
-    return (nmatrix)
+    new_matrix = matrix.copy()
+
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+
+    return (new_matrix)
