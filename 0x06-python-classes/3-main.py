758ff0cf7b7ee0f15cfb894002a4b5b43b3c7f75
mrfaraa <elmehdifaraa@gmail.com>
2023-06-27 09:07:26 -0700

    task 3 update area of a square

--- 0x06-python-classes/3-main.py
+++ 0x06-python-classes/3-main.py
@@ -1,35 +1,18 @@
 #!/usr/bin/python3
-"""defines a class called square"""
+Square = __import__('3-square').Square
 
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
 
-class Square:
-    """Initialize a square
-    Args:
-        __size (int): size of the square
-    """
-    def __init__(self, size=0):
-        """Initialize a square
-        Args:
-            size (int): size of the square
-        Returns: None
-        """
-        self.size = size
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
 
-    @property
-    def size(self):
-        """getter of the size"""
-        return self.__size
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
 
-    @size.setter
-    def size(self, value):
-        """sets the value for size"""
-        if isinstance(value, int):
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
-        else:
-            raise TypeError("size must be an integer")
-
-    def area(self):
-        """This computes the area of the square"""
-        return (self.__size ** 2)
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
