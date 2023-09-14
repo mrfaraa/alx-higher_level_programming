e1a66a51fbad21b9cd4ce8a084adb228a763908b
mrfaraa <elmehdifaraa@gmail.com>
2023-09-14 07:34:59 -0700

    update

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+File: 0-select_states.py
+Desc: a script that lists all states from the database hbtn_0e_0_usa
+Author: EL Mehdi Faraa (mrfaraa)
+Date Created: 14 September 2023
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+Python - Object-relational mapping: Tasks a project written in Python programming language, 15 mandatory tasks and 3 advanced tasks.
